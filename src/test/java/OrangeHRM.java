bdbfda9334543bc91388d1e22ee6f8d0f5169ee8
sivsanka <siva29042003@gmail.com>
2024-11-16 19:16:38 +0530

    commit new scripts

--- src/test/java/OrangeHRM.java
+++ src/test/java/OrangeHRM.java
@@ -1,6 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,9 +12,10 @@ public class OrangeHRM
 {
     WebDriver driver;
 
-    @BeforeMethod
+
+    @BeforeClass
     void preconditions(){
-        driver=new ChromeDriver();
+        driver=new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
@@ -34,9 +37,41 @@ public class OrangeHRM
         driver.findElement(By.xpath("//span[text()='Performance']")).click();
         driver.findElement(By.xpath("//span[text()='Dashboard']")).click();
         driver.findElement(By.xpath("//span[text()='Directory']")).click();
-        driver.findElement(By.xpath("//span[text()='Maintenance']")).click();
         driver.findElement(By.xpath("//span[text()='Claim']")).click();
         driver.findElement(By.xpath("//span[text()='Buzz']")).click();
 
     }
+    @Test(priority = 3)
+    void VerifyAbleToCheckMaintance() {
+        driver.findElement(By.xpath("//span[text()='Maintenance']")).click();
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[text()=' Confirm ']")).click();
+    }
+   @Test(priority = 4)
+    void VerifyAbleToDownloadPersonalData() throws InterruptedException {
+        driver.findElement(By.xpath("//a[text()='Access Records']")).click();
+        WebElement Serach = driver.findElement(By.xpath("//input[@placeholder='Type for hints...']"));
+        Serach.sendKeys("siva");
+        Thread.sleep(2000);
+        Serach.sendKeys(Keys.ARROW_DOWN);
+        Serach.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//button[text()=' Search ']")).click();
+        driver.findElement(By.xpath("//button[text()=' Download ']")).click();
+    }
+    @Test(priority = 5)
+    void VerifyAllOptionDisplayingAdminTab(){
+        driver.findElement(By.xpath("//span[text()='Admin']")).click();
+        driver.findElement(By.xpath("//span[text()='User Management ']")).click();
+        driver.findElement(By.xpath("//span[text()='Job ']")).click();
+        driver.findElement(By.xpath("//span[text()='Organization ']")).click();
+        driver.findElement(By.xpath("//span[text()='Qualifications ']")).click();
+        driver.findElement(By.xpath("//a[text()='Nationalities']")).click();
+        driver.findElement(By.xpath("//a[text()='Corporate Branding']")).click();
+        driver.findElement(By.xpath("//span[text()='Configuration ']")).click();
+    }
+   @AfterClass
+    void PostConditions(){
+        driver.quit();
+    }
+
 }
